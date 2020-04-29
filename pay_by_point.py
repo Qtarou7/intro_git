6d2800c6b5700f06876cafcb294130f57859786c
Qtarou7 <zzzxxxvvv99@gmail.com>
2020-04-29 20:16:18 +0900

    add main routine

--- pay_by_point.py
+++ pay_by_point.py
@@ -2,23 +2,27 @@
 # 自分の得意な言語で
 # Let's チャレンジ！！
 
-get_money = input().split()
-#print(get_money)
+def main():
+    get_money = input().split()
+    #print(get_money)
 
-money = int(get_money[0])
-point = int(0)
-loop = int(get_money[1])
+    money = int(get_money[0])
+    point = int(0)
+    loop = int(get_money[1])
 
-import sys
-line = sys.stdin.readlines()
-#print(line)
+    import sys
+    line = sys.stdin.readlines()
+    #print(line)
 
-for i in range(loop):
-    use = int(line[i])
-    if use <= point:
-        point -= use
-    else:
-        money -= use
-        point += int(use/10)
-    print(money,point)
-    i += 1
+    for i in range(loop):
+        use = int(line[i])
+        if use <= point:
+            point -= use
+        else:
+            money -= use
+            point += int(use/10)
+        print(money,point)
+        i += 1
+
+if __name__ == "__main__":
+main()
