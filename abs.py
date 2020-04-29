4fda4d8e15c74a8e221c480b7e3dcd3131bc77fb
Qtarou7 <50232806+Qtarou7@users.noreply.github.com>
2020-04-29 16:33:29 +0900

    Create abs.py
    絶対値を求めるソフト

---
+++ abs.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+# 標準入力から値を取得してinput_lineに入れる
+
+# 取得した値を標準出力するために"XXXXXX"をどう書き換える？
+
+def main():
+input_line = input()
+print(abs(int(input_line)))
+
+if __name__ == "__main__":
+main()
